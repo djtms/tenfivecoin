d11741c505817054611313200920f35121468aa8
icanprogram <icanprogramfedoracoins@gmail.com>
2014-03-17 18:30:01 +0100

    Hard fork at 4000, rewards step back to pi

--- src/main.cpp
+++ src/main.cpp
@@ -1115,13 +1115,8 @@ int64 static GetBlockValue(int nHeight, int64 nFees, uint256 prevHash)
     if      (nHeight == 1) { nSubsidy = nPremineAmount * COIN; }   /* Premine Allocation */
     else if (nHeight <= 1050) { nSubsidy = 1.57079632 * COIN; } /* 1050 */
     else if (nHeight <= 3150) { nSubsidy = 3.14159265 * COIN; } /* 1050 * 3 */
-    else if (nHeight <= 6300) { nSubsidy = 9.8596 * COIN; }        /* 1050 * 6 */
-    else if (nHeight <= 9450) { nSubsidy = 19.7192 * COIN; }       /* 1050 * 9 */
-    else if (nHeight <= 18900) { nSubsidy = 25 * COIN; }           /* 1050 * 18 */
-    else if (nHeight <= 37800) { nSubsidy = 50 * COIN; }           /* 1050 * 36 */
-    else if (nHeight <= 75600) { nSubsidy = 25 * COIN; }           /* 1050 * 72 */
-    else if (nHeight <= 151200) { nSubsidy = 19.7192 * COIN; }      /* 1050 * 144 */
-    else { nSubsidy = 9.8596 * COIN; }
+    else if (nHeight <= 4000) { nSubsidy = 9.8596 * COIN; }        /* 1050 * 6 */
+    else { nSubsidy = 3.14159265 * COIN; }
     
     return nSubsidy + nFees;
 }
